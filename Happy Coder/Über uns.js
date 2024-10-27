6f31277fe6053fade6f404398a8a98980f66e239
verbesserungslos <ibimsverbesserungslos@gmail.com>
2024-10-27 14:36:09 +0100

    Add files via upload

---
+++ Happy Coder/Über uns.js
@@ -0,0 +1,6 @@
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+        crossOrigin="map"></script>
+
+
+var map = L.map('map').setView([51.505, -0.09], 13);
